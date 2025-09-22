471d039887ea31d091063f5b8324db80c2ccf89d
JesusTiburzio <jesusath@alianz.edu.mx>
2025-09-22 10:07:51 -0600

    Create EJERCICIO 6.c

---
+++ EJERCICIO 6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int count_pulses(int signals[], int size);
+
+// Test case en main()
+int main() {
+    int test1[] = {0, 1, 0, 1, 0, 1, 0};
+    int test2[] = {1, 1, 1, 1, 1};
+    int test3[] = {0, 0, 0, 0};
+    
+    printf("Test 1: count_pulses(test1, 7) = %d (esperado: 3)\n", count_pulses(test1, 7));
+    printf("Test 2: count_pulses(test2, 5) = %d (esperado: 5)\n", count_pulses(test2, 5));
+    printf("Test 3: count_pulses(test3, 4) = %d (esperado: 0)\n", count_pulses(test3, 4));
+    return 0;
+}
+
+int count_pulses(int signals[], int size){
+
+    for (int i = 0; i < size; i++){
+        if (signals[i]==1){
+            return signals[i];
+        }
+
+    }
+
+}
